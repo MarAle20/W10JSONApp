c42013406361ecc0d0a20824cb2dbb622771cbcc
Marian <alejanma@sheridancollege.ca>
2023-11-13 21:16:56 +0000

    Initial Commit

---
+++ W10JSONApp/W10JSONAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  W10JSONAppApp.swift
+//  W10JSONApp
+//
+//  Created by Default User on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct W10JSONAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
